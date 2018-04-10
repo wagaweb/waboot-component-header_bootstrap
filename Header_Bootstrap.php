18b44021f777a0ff2d4c7e23c35cb3a7004519d7
LostCore <lostcore@gmail.com>
2018-04-10 11:12:30 +0200

    Standardized version

--- Header_Bootstrap.php
+++ Header_Bootstrap.php
@@ -4,7 +4,7 @@ Component Name: Header Bootstrap
 Description: Header Bootstrap Component
 Category: Layout
 Tags: Header
-Version: 1.0
+Version: 1.0.0
 Author: WAGA Team <dev@waga.it>
 Author URI: http://www.waga.it
 */
